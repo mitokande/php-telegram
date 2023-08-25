1d623f2a3c61d1e4207753c1fcfe5252afa8417c
mitokande <benmithat18@gmail.com>
2023-08-25 22:23:17 +0300

    feat(SendTelegramMessage):method for sending message

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -2,6 +2,8 @@
 
 namespace Mitokande\PhpTelegram;
 
+use Mitokande\PhpTelegram\TelegramMessage;
+
 class TelegramBot
 {
 
@@ -12,5 +14,28 @@ class TelegramBot
         $this->TelegramBotToken = $telegram_bot_token;
     }
 
+    public function SendTelegramMessage(TelegramMessage $telegram_message): TelegramMessage
+    {
+        $apiUrl = "https://api.telegram.org/bot{$this->TelegramBotToken}/sendMessage";
+        $data = array(
+            'chat_id' => $telegram_message->chatID,
+            'text' => $telegram_message->TelegramMessage,
+        );
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'content' => http_build_query($data),
+                'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+            )
+        );
+        $context = stream_context_create($options);
+        $result = file_get_contents($apiUrl, false, $context);
+
+        if ($result === false) {
+            return $telegram_message;
+        } else {
+            return $telegram_message;
+        }
+    }
 
 }
